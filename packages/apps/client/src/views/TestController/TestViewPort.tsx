2b363db0ed7b1aa9af4a4f82b3d1e35d07f331ec
Jan Starzak <jan.starzak@gmail.com>
2024-02-12 11:15:21 +0100

    chore: fix TestViewPort to make it pass linting

--- packages/apps/client/src/views/TestController/TestViewPort.tsx
+++ packages/apps/client/src/views/TestController/TestViewPort.tsx
@@ -32,7 +32,7 @@ export const TestViewPort: React.FC<{ api: APIConnection }> = ({ api }) => {
 
 			// Also fetch initial settings:
 			api.viewPort
-				.get('')
+				.get(null)
 				.then((data) => {
 					setViewPort(data)
 				})
@@ -53,7 +53,7 @@ export const TestViewPort: React.FC<{ api: APIConnection }> = ({ api }) => {
 					<EditObject
 						obj={viewPort}
 						onChange={(newViewPort) => {
-							api.viewPort.update('', newViewPort).catch(console.error)
+							api.viewPort.update(null, newViewPort).catch(console.error)
 						}}
 					/>
 				</div>
