93c693f8852763b3b9dada67f7b85f6f4b230aa0
Johan Nyman <johan@nytamin.se>
2024-02-28 08:34:44 +0100

    fix: move control subscription to ControlStore

--- packages/apps/client/src/stores/ControlStore.ts
+++ packages/apps/client/src/stores/ControlStore.ts
@@ -1,8 +1,20 @@
-import { PartId, SegmentId, TextMarkerId } from '@sofie-prompter-editor/shared-model'
+import { ControllerMessage, PartId, SegmentId, TextMarkerId } from '@sofie-prompter-editor/shared-model'
 import { APIConnection, RootAppStore } from './RootAppStore'
+import { IReactionDisposer, action } from 'mobx'
+import EventEmitter from 'eventemitter3'
 
-export class ControlStore {
-	constructor(public appStore: typeof RootAppStore, public connection: APIConnection) {}
+interface ControlStoreEvents {
+	message: [ControllerMessage]
+}
+export class ControlStore extends EventEmitter<ControlStoreEvents> {
+	initialized = false
+	private initializing = false
+
+	reactions: IReactionDisposer[] = []
+
+	constructor(public appStore: typeof RootAppStore, public connection: APIConnection) {
+		super()
+	}
 
 	jumpToObject(objectId: SegmentId | PartId | TextMarkerId, offset: number = 0): void {
 		console.log('jumpToObject', objectId)
@@ -12,9 +24,35 @@ export class ControlStore {
 					target: objectId,
 					offset,
 				},
-				speed: null,
 			})
 			.then(() => console.log('sent!'))
 			.catch(console.error)
+	}
+	public initialize() {
+		if (this.initializing || this.initialized) return
+		this.initializing = true
+
+		this.setupSubscription()
+	}
+
+	private setupSubscription = action(() => {
+		this.reactions.push(
+			this.appStore.whenConnected(async () => {
+				// Setup subscription and NOT load initial data:
+				RootAppStore.connection.controller.subscribeToMessages().catch(console.error)
+
+				this.initialized = true
+			})
+		)
+
+		RootAppStore.connection.controller.on('message', (message) => {
+			console.log('MESSAGEYO', message)
+			this.emit('message', message)
+		})
+	})
+	dispose() {
+		for (const dispose of this.reactions) {
+			dispose()
+		}
 	}
 }
